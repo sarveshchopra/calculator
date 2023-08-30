893f523a1ce5cbe83bc66b37a9f373983a8e7886
sarveshchopra <72998328+sarveshchopra@users.noreply.github.com>
2023-08-30 10:30:58 +0530

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,6 @@
+a = 10
+b = 3
+game.splash("Addition is : " + str((a + b)))
+game.splash("Subtraction is: " + str((a - b)))
+game.splash("Multiplication is: " + str(a * b))
+game.splash("Division is: " + str(a / b))
---
+++ main.ts
@@ -0,0 +1,6 @@
+let a = 10
+let b = 3
+game.splash("Addition is : " + (a + b))
+game.splash("Subtraction is: " + (a - b))
+game.splash("Multiplication is: " + a * b)
+game.splash("Division is: " + a / b)
