10dfbf114c3e4ea1d281ae0aa925fb504e27ec38
sarveshchopra <72998328+sarveshchopra@users.noreply.github.com>
2023-08-31 16:27:58 +0530

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 a = 10
 b = 3
+c=a/b
 game.splash("Addition is : " + str((a + b)))
 game.splash("Subtraction is: " + str((a - b)))
 game.splash("Multiplication is: " + str(a * b))
-game.splash("Division is: " + str(a / b))
+game.splash("Division is: " + str(Math.round(c)))
--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 let a = 10
 let b = 3
-game.splash("Addition is : " + (a + b))
-game.splash("Subtraction is: " + (a - b))
-game.splash("Multiplication is: " + a * b)
-game.splash("Division is: " + a / b)
+let c = a / b
+game.splash("Addition is : " + ("" + (a + b)))
+game.splash("Subtraction is: " + ("" + (a - b)))
+game.splash("Multiplication is: " + ("" + a * b))
+game.splash("Division is: " + ("" + Math.round(c)))
